6bcf683f089b83713ede55a7dc9d55d6d19db170
ItzzHayden17 <119879737+ItzzHayden17@users.noreply.github.com>
2026-01-29 15:29:30 +0200

    Serving public folder

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 const port = 8080
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+app.use("/public", express.static("public"));
 // const corsOptions = {
 //     origin: function (origin,callback){
 //         const allowedOrigin = /^(https?:\/\/)?([\w-]+\.)*amplifin\.co\.za$/
@@ -94,5 +94,6 @@ app.get("/live.js", (req, res) => {
 .listen(port ,()=>{
     console.log(`Listening on port ${port}`);
 })
+
 
 
